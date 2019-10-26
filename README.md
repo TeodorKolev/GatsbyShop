11e20615fac0ef6bb0e78f3689d4d527b3a70854
Teodor Kolev <teodorkolev007@gmail.com>
2019-10-26 17:40:39 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+  Live Demo:
+  https://todo.com
+
+
+  ## 🚀 Quick start
+
+  1.  **Setup Contentful Models**
+
+      Use [contentful-cli](https://github.com/contentful/contentful-cli) to import the models from oneshopper.json
+
+      ```
+      contentful space --space-id <CONTENTFUL_SPACE_ID> import --content-file oneshopper.json
+      ```
+
+  1.  **Start developing.**
+
+      Navigate into your new site’s directory and start it up.
+
+      ```sh
+      cd OneShopper
+      npm install
+      gatsby develop
+      ```
+
+  1.  **Setup your Own Configure Projects.**
+
+      Enter your own key
+
+      [ContentFul](https://be.contentful.com/login):
+      - spaceId = **Key**
+      - accessToken = **Key**
+
+      [snipcart](https://app.snipcart.com/):
+      - snipcart = **Key**
+  <!-- AUTO-GENERATED-CONTENT:END -->
---
+++ src/components/header.js
@@ -0,0 +1,61 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import "../css/font-awesome.css"
+import "bootstrap/dist/css/bootstrap.css"
+import "../css/style.css"
+import SEO from "../components/seo"
+import logo from "../images/oneshopper-logo.png"
+
+
+
+const Header = ({ siteTitle }) => (
+  <header className="site-header">
+    <SEO>
+    </SEO>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12 col-md-4 align-self-center">
+          <Link className="header-logo" to="/"><img src={logo}></img></Link>
+        </div>
+        <div className="col-sm-12 col-md-8 align-self-center">
+          <nav>
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/blogs">Blogs</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/store">Store</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact-us">Contact</Link>
+              </li>
+            </ul>
+            <div className="header-cart">
+              <Link className="Header__summary snipcart-summary snipcart-checkout" to="#">
+                <i className="fas fa-cart-plus"></i>
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+  </header >
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
