6626f7868bab81f87b1d500f8fe9618b0b06c03a
Teodor Kolev <teodorkolev007@gmail.com>
2019-11-02 02:44:07 +0200

    revert 4 commits back master

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,47 +1,46 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class BlogsPost extends React.Component {
+class BlogsPost extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      NoOfPost: 6
-    };
-    this.handleScroll = this.handleScroll.bind(this);
+      NoOfPost: 6,
+    }
+    this.handleScroll = this.handleScroll.bind(this)
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener("scroll", this.handleScroll)
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener("scroll", this.handleScroll)
   }
 
   handleScroll = () => {
-    var lastScrollY = window.pageYOffset + 1100;
+    var lastScrollY = window.pageYOffset + 1100
 
     if (lastScrollY > window.outerHeight) {
-      var count = this.state.NoOfPost + 3;
+      var count = this.state.NoOfPost + 3
       this.setState({
-        NoOfPost: count
-      });
+        NoOfPost: count,
+      })
     }
-  };
+  }
 
   render() {
-
-    const { data } = this.props;
-    const { NoOfPost } = this.state;
+    const { data } = this.props
+    const { NoOfPost } = this.state
 
     return (
       <React.Fragment>
         <ul className="blog-list" onScroll={this.onScrollEvent}>
           {data.data.allContentfulBlogs.edges.slice(0, NoOfPost).map(items => (
-            <li>
+            <li key={items.node.id}>
               <div className="post-item template-square columned">
                 <div className="post-thumbnail">
                   <Img sizes={items.node.featureImage.fluid} />
@@ -51,25 +50,25 @@ class BlogsPost extends React.Component {
                   </div>
                 </div>
                 <div className="post-details">
-                  <h2 className="post-title"><Link to={`/${items.node.slug}`}>{items.node.title}</Link></h2>
+                  <h2 className="post-title">
+                    <Link to={`/${items.node.slug}`}>{items.node.title}</Link>
+                  </h2>
                   <div className="author">
                     <Img sizes={items.node.author.photo.fluid} />
                     <strong className="name">{items.node.author.name}</strong>
                   </div>
                   <p>{items.node.description.childMarkdownRemark.excerpt}</p>
-
                 </div>
               </div>
             </li>
           ))}
         </ul>
       </React.Fragment>
-    );
+    )
   }
 }
 
 const Blogs = data => (
-
   <Layout>
     <SEO title="Blogs" keywords={[`gatsby`, `application`, `react`]} />
     <div className="container blog-page">
@@ -83,33 +82,16 @@ export default Blogs
 export const query = graphql`
   query BlogsQuery {
     allContentfulBlogs {
-        edges {
-          node {
-            id
-            title
-            slug
-            publicData(formatString: "MMMM D, YYYY")
-            author {
-              name
-              photo {
-                fluid(maxWidth: 350) {
-                  base64
-                  aspectRatio
-                  src
-                  srcSet
-                  srcWebp
-                  srcSetWebp
-                  sizes
-                }
-              }
-            }
-            description {
-              childMarkdownRemark {
-                excerpt(pruneLength: 250)
-              }
-            }
-            featureImage {
-              fluid(maxWidth: 1120) {
+      edges {
+        node {
+          id
+          title
+          slug
+          publicData(formatString: "MMMM D, YYYY")
+          author {
+            name
+            photo {
+              fluid(maxWidth: 350) {
                 base64
                 aspectRatio
                 src
@@ -120,10 +102,24 @@ export const query = graphql`
               }
             }
           }
+          description {
+            childMarkdownRemark {
+              excerpt(pruneLength: 250)
+            }
+          }
+          featureImage {
+            fluid(maxWidth: 1120) {
+              base64
+              aspectRatio
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+              sizes
+            }
+          }
         }
       }
+    }
   }
 `
-
-
-
